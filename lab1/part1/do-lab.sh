5ccced16bbadc995086cdd65c23ed5331e614e4f
Phuong Dao <phuong018@gmail.com>
2024-05-18 20:16:02 +0000

    Lab1: Creating bridge

--- lab1/part1/do-lab.sh
+++ lab1/part1/do-lab.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+sw="docker exec -it clab-lab1-part1-switch"
+
 # INCLUDE ALL COMMANDS NEEDED TO PERFORM THE LAB
 # This file will get called from capture_submission.sh
-
+$sw ip link add labbridge type bridge
+$sw ip link set labbridge up
+$sw ip link set eth1 master labbridge
+$sw ip link set eth2 master labbridge
+$sw ip link set eth3 master labbridge
+$sw ip link set eth4 master labbridge
